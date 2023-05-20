3b303af474c10d09850134ce6a4828abead02141
neadianyanwu <neadianyanwu@gmail.com>
2023-05-19 14:32:09 -1100

    edited navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./Navbar.css";
 
 import React, { Component } from 'react'
 
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <h1 className="logo">
-          <a href="/home" className="navlink" aria-current="page">Krypto</a>
-        </h1>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-        aria-aria-label="Toggle-navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li  className="nav-item">
-              <a href="/about" className="navlink">About</a>
-            </li>
-            <li  className="nav-item">
-              <a href="/services" className="navlink">Services</a>
-            </li>
-            <li  className="nav-item">
-              <a href="/portfolio" className="navlink">Portfolio</a>
-            </li>
-            <li  className="nav-item">
-              <a href="/contacts" className="navlink">Contacts</a>
-            </li>
+   <section>
+        <header id="header" className="fixed-top">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <h1 className="logo mr-auto"><a class="navbar-brand" href="#">Krypto</a></h1>
+              <button className="navbar-toggler d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="toggler-icon top-bar"></span>
+                <span className="toggler-icon middle-bar"></span>
+                <span className="toggler-icon bottom-bar"></span> 
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Portfolio</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Services</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                  </li>
+                </ul>
 
-            <button type="button" className="btn btn-dark">Get Started</button>
-          </ul>
-        </div>
-      </div>
-    </nav>
+                <a href="#" className="get-started-btn scrollto">Get Started</a>
+              </div>
+            </div>
+          </nav>
+        </header>
+   </section>
   )
 }
 
